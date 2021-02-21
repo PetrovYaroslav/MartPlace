662394935d400615a9c56e7f1ffbff9dd2bcb992
PetrovYaroslav <petrov.yaroslav@gmail.com>
2021-02-21 19:13:09 +0200

    added section features

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+  $('.features__slider-wrapper').slick({
+    arrows: true,
+    prevArrow: '<button class="slick-arrow slick-prev"> <img src="images/features/arrow-left.svg" alt="prev"> </button>',
+    nextArrow: '<button class="slick-arrow slick-next"> <img src="images/features/arrow-right.svg" alt="next"> </button>',
+
+  });
+
+
+
+
+  $(".features__item-star").rateYo({
+    rating: 4.5,
+    starWidth: "17px",
+    readOnly: true
+  });
+
 });
